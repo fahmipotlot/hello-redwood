b605c726adcdf0a761bd8f6ef75ad95e82fc49cc
fahmi amir <amirfahmi8@gmail.com>
2020-12-01 08:19:39 +0700

    initial commit

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,11 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <p>Full stack web developer</p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const HomePage = () => {
+  return (
+    <BlogLayout>
+      <p><a href="https://www.instagram.com/fahmipotlot">IG</a></p>
+      <p><a href="https://twitter.com/fahmipotlot">Twitter</a></p>
+      <p><a href="https://github.com/fahmipotlot">GitHub</a></p>
+    </BlogLayout>
+  )
+}
+
+export default HomePage
